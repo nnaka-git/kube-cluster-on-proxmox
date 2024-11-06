79dd70c85513dac6034b3b310d490ffd0e712e3b
nnaka-git <norio.nakamura@gmail.com>
2024-11-06 20:27:22 +0900

    new file:   deploy.sh 	new file:   init.sh

---
+++ deploy.sh
@@ -0,0 +1,136 @@
+#!/usr/bin/env bash
+
+# region : set variables
+
+TEMPLATE_VMID=900
+CLOUDINIT_IMAGE_TARGET_VOLUME=local-lvm
+TEMPLATE_BOOT_IMAGE_TARGET_VOLUME=local-lvm
+BOOT_IMAGE_TARGET_VOLUME=local-lvm
+SNIPPET_TARGET_VOLUME=local
+SNIPPET_TARGET_PATH=/var/lib/vz/snippets
+VM_DISK_IMAGE=/var/lib/vz/images/AlmaLinux-9-GenericCloud-latest.x86_64.qcow2
+VM_LIST=(
+    # ---
+    # vmid:       proxmox上でVMを識別するID
+    # vmname:     proxmox上でVMを識別する名称およびホスト名
+    # cpu:        VMに割り当てるコア数(vCPU)
+    # mem:        VMに割り当てるメモリ(MB)
+    # vmsrvip:    VMのService Segment側NICに割り振る固定IP
+    # ---
+    #vmid #vmname #cpu #mem  #vmsrvip    
+    "1121 k8s-cp1 2    4096  192.168.1.121"
+    "1122 k8s-wk1 4    8192  192.168.1.122"
+    "1123 k8s-wk2 4    8192  192.168.1.123"
+)
+
+# endregion
+
+# ---
+
+# region : create template-vm
+
+# create a new VM and attach Network Adaptor
+# vmbr0=Service Network Segment (192.168.1.0/24)
+qm create $TEMPLATE_VMID --bios seabios --cpu x86-64-v2-AES --cores 2 --memory 4096 --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-single --ostype l26 --name k8s-template
+
+# import the downloaded disk to $TEMPLATE_BOOT_IMAGE_TARGET_VOLUME storage
+qm set $TEMPLATE_VMID --scsi0 local-lvm:0,import-from=${VM_DISK_IMAGE},format=qcow2,cache=writeback,discard=on
+
+# add Cloud-Init CD-ROM drive
+qm set $TEMPLATE_VMID --ide0 $CLOUDINIT_IMAGE_TARGET_VOLUME:cloudinit
+
+# set the bootdisk parameter to scsi0
+qm set $TEMPLATE_VMID --boot c --bootdisk scsi0
+
+# set serial console
+#qm set $TEMPLATE_VMID --serial0 socket --vga serial0
+
+# migrate to template
+qm template $TEMPLATE_VMID
+
+# endregion
+
+# ---
+
+# region : setup vm from template-vm
+
+for array in "${VM_LIST[@]}"
+do
+	echo "${array}" | while read -r vmid vmname cpu mem vmsrvip 
+	do
+		# clone from template
+		qm clone "${TEMPLATE_VMID}" "${vmid}" --name "${vmname}" --full true
+		
+		# set compute resources
+		qm set "${vmid}" --cores "${cpu}" --memory "${mem}"
+
+		# resize disk (Resize after cloning, because it takes time to clone a large disk)
+		qm resize "${vmid}" scsi0 60G
+
+		# create snippet for cloud-init(user-config)
+		cat > "$SNIPPET_TARGET_PATH"/"$vmname"-user.yaml <<- EOF
+			#cloud-config
+			# SYSTEM
+			hostname: ${vmname}
+			fqdn: ${vmname}.blue-screen.mydns.jp
+			manage_etc_hosts: true
+			locale: ja_JP.UTF-8
+			timezone: Asia/Tokyo
+			# USER
+			users:
+			- default
+			- name: red
+			  lock_passwd: false
+			  # mkpasswd --method=SHA-512 --rounds=4096
+			  passwd: \$6\$rounds=4096\$2dcpst67UO5pMw7H\$OlPx45objlhjmlFx7dj0/BA/Bv/JVI/z6xNNjRr/7wwqAEgi8XjROA8f/WCoiPnaTSz.P6OMKtLNyq4jTrHnq0
+			  shell: /bin/bash
+			  sudo: ALL=(ALL) NOPASSWD:ALL
+			  uid: 1000
+			expire: False
+			package_upgrade: true
+			runcmd:
+			  # set ssh_authorized_keys
+			  - su - red -c "mkdir -p ~/.ssh && chmod 700 ~/.ssh"
+			  - su - red -c "curl -sS https://github.com/nnaka-git.keys >> ~/.ssh/authorized_keys"
+			  - su - red -c "chmod 600 ~/.ssh/authorized_keys"
+			# REBOOT
+			power_state:
+			  mode: reboot
+		EOF
+
+		# create snippet for cloud-init(network-config)
+		cat > "$SNIPPET_TARGET_PATH"/"$vmname"-network.yaml <<- EOF
+			version: 1
+			config:
+			  - type: physical
+			    name: eth0
+			    subnets:
+			      - type: static
+			        address: ${vmsrvip}/24
+			        gateway: 192.168.1.1
+			  - type: nameserver
+			    address:
+			      - 192.168.1.1
+			      - 8.8.8.8
+			    search:
+			      - local
+		EOF
+
+		# set snippet to vm
+		qm set "${vmid}" --cicustom "user=${SNIPPET_TARGET_VOLUME}:snippets/${vmname}-user.yaml,network=${SNIPPET_TARGET_VOLUME}:snippets/${vmname}-network.yaml"
+
+	done
+done
+
+for array in "${VM_LIST[@]}"
+do
+    echo "${array}" | while read -r vmid vmname cpu mem vmsrvip
+    do
+        # start vm
+        qm start "${vmid}"
+        
+    done
+done
+
+# endregion
+
---
+++ init.sh
@@ -0,0 +1,7 @@
+qm stop 1121
+qm stop 1122
+qm stop 1123
+qm destroy 1121
+qm destroy 1122
+qm destroy 1123
+qm destroy 900
