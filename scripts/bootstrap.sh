2e35c815ebf889c40cb9ff25a34d9f0eb4a66e7c
nnaka-git <norio.nakamura@gmail.com>
2024-11-14 23:04:50 +0900

    modified:   scripts/bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,6 +2,6 @@
 # Install Ansible
 sudo dnf install -y ansible-core
 # Clone your Ansible repository
-# git clone https://github.com/yourusername/ansible-config.git /etc/ansible
+git clone https://github.com/nnaka-git/kube-cluster-on-proxmox.git "$HOME"/kube-cluster-on-proxmox
 # Run your initial Ansible playbook
 # ansible-playbook /etc/ansible/playbooks/initial-setup.yml
