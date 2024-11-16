3a5d8b724bb7c0f66392dc7b094d0c74eb04ed62
nnaka-git <norio.nakamura@gmail.com>
2024-11-17 03:18:38 +0900

    modified:   ansible/hosts/kube-servers/inventory 	modified:   scripts/bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -7,5 +7,5 @@ git clone https://github.com/nnaka-git/kube-cluster-on-proxmox.git "$HOME"/kube-
 # export ansible.cfg target
 export ANSIBLE_CONFIG="$HOME"/kube-cluster-on-proxmox/ansible/ansible.cfg
 ansible-galaxy role install -r "$HOME"/kube-cluster-on-proxmox/ansible/roles/requirements.yaml
-ansible-galaxy collection install -r "$HOME"/kube-cluster-on-proxmox/ansible/roles/requirements.yamll
-ansible-playbook "$HOME"/kube-cluster-on-proxmox/ansible/clusterwide.yam
+ansible-galaxy collection install -r "$HOME"/kube-cluster-on-proxmox/ansible/roles/requirements.yaml
+ansible-playbook "$HOME"/kube-cluster-on-proxmox/ansible/clusterwide.yaml
