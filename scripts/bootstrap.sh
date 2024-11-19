13971f64b6e45d6f9fbf16ece32dcc9eb5ac0348
nnaka-git <norio.nakamura@gmail.com>
2024-11-19 23:39:01 +0900

    modified:   ansible/ansible.cfg 	deleted:    ansible/clusterwide.yaml 	renamed:    ansible/hosts/kube-servers/inventory -> ansible/hosts/inventory 	new file:   ansible/kube-setup.yaml 	deleted:    ansible/roles/01-ssh-keygen/tasks/main.yaml 	deleted:    ansible/roles/02-setup-sshd-config/tasks/main.yaml 	deleted:    ansible/roles/requirements.yaml 	modified:   scripts/bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,7 +6,4 @@ git clone https://github.com/nnaka-git/kube-cluster-on-proxmox.git "$HOME"/kube-
 # Run your initial Ansible playbook
 # export ansible.cfg target
 export ANSIBLE_CONFIG="$HOME"/kube-cluster-on-proxmox/ansible/ansible.cfg
-ansible-galaxy collection install community.crypto
-ansible-galaxy role install -r "$HOME"/kube-cluster-on-proxmox/ansible/roles/requirements.yaml
-ansible-galaxy collection install -r "$HOME"/kube-cluster-on-proxmox/ansible/roles/requirements.yaml
 ansible-playbook "$HOME"/kube-cluster-on-proxmox/ansible/clusterwide.yaml
