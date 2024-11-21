e52ad07011e396fa3039b5760f8647470eec87cc
nnaka-git <norio.nakamura@gmail.com>
2024-11-21 21:40:45 +0900

    modified:   ansible/hosts/inventory 	modified:   ansible/kube-setup.yaml 	modified:   ansible/roles/01-ssh-keygen/tasks/main.yaml 	modified:   ansible/roles/requirements.yaml 	modified:   scripts/bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,4 +6,6 @@ git clone https://github.com/nnaka-git/kube-cluster-on-proxmox.git "$HOME"/kube-
 # Run your initial Ansible playbook
 # export ansible.cfg target
 export ANSIBLE_CONFIG="$HOME"/kube-cluster-on-proxmox/ansible/ansible.cfg
-ansible-playbook "$HOME"/kube-cluster-on-proxmox/ansible/kube-setup.yaml
+ansible-galaxy role install -r "$HOME"/kube-cluster-on-proxmox/ansible/roles/requirements.yaml
+ansible-galaxy collection install -r "$HOME"/kube-cluster-on-proxmox/ansible/roles/requirements.yaml
+# ansible-playbook "$HOME"/kube-cluster-on-proxmox/ansible/kube-setup.yaml
