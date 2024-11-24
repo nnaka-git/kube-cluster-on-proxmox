6779bd43c720a1f93863a3dc084a70b5830bcb4f
nnaka-git <norio.nakamura@gmail.com>
2024-11-24 23:01:48 +0900

    modified:   deploy.sh 	modified:   scripts/bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
+# Exit except control-plane
+if [$1 = "kube-cp1"]; then
+    ;;
+else
+    exit 0
+if
+
 # Install Ansible
 sudo dnf install -y ansible-core
+
 # Clone your Ansible repository
 git clone https://github.com/nnaka-git/kube-cluster-on-proxmox.git "$HOME"/kube-cluster-on-proxmox
+
 # Run your initial Ansible playbook
 # export ansible.cfg target
 export ANSIBLE_CONFIG="$HOME"/kube-cluster-on-proxmox/ansible/ansible.cfg
+
+# ansible-galaxy
 ansible-galaxy role install -r "$HOME"/kube-cluster-on-proxmox/ansible/roles/requirements.yml
 ansible-galaxy collection install -r "$HOME"/kube-cluster-on-proxmox/ansible/roles/requirements.yml
+# ansible-playbook
 ansible-playbook -vvvv "$HOME"/kube-cluster-on-proxmox/ansible/kube-setup.yml
