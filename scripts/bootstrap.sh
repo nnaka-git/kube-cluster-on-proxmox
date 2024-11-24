a649728a4c8455570a26fcaa0ba9e365d9f124c1
nnaka-git <norio.nakamura@gmail.com>
2024-11-24 22:40:43 +0900

    modified:   scripts/bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -8,4 +8,4 @@ git clone https://github.com/nnaka-git/kube-cluster-on-proxmox.git "$HOME"/kube-
 export ANSIBLE_CONFIG="$HOME"/kube-cluster-on-proxmox/ansible/ansible.cfg
 ansible-galaxy role install -r "$HOME"/kube-cluster-on-proxmox/ansible/roles/requirements.yml
 ansible-galaxy collection install -r "$HOME"/kube-cluster-on-proxmox/ansible/roles/requirements.yml
-ansible-playbook "$HOME"/kube-cluster-on-proxmox/ansible/kube-setup.yml
+ansible-playbook -vvvv "$HOME"/kube-cluster-on-proxmox/ansible/kube-setup.yml
